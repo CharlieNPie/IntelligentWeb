71ba4e3abc382bb3be1bfdf68edcfa513b662769
borjadotai <hi@borja.ai>
2019-03-19 20:04:44 +0000

    intial

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,260 +1,267 @@
 var data = [
-    {
-      name: "Coachella",
-      date: "12/05/2019",
-      image: "http://google.io/picture.jpeg",
-      organiser: "93729347234",
-      location: "Los Angeles",
-      posts: [
-        {
-          text: "Lol I just lost my cap, anybody seen it?",
-          image: "http://borja.leiva/image.jpeg",
-          author: "borjadotai",
-          date: "12/05/2019 - 17:30",
-          location: "Los Angeles - 3rd Area",
-          comments: [
-            {
-              author: "hasanasim",
-              text: "Yo yo I saw it in Michelles crib"
-            },
-            {
-              author: "michelle23",
-              text: "Yeah its here! Come get it."
-            }
-          ]
-        },
-        {
-          text:
-            "What time is Travis playing at? Completely lost track of time...",
-          image: "http://borja.leiva/image.jpeg",
-          author: "hasanasim",
-          date: "12/05/2019 - 14:30",
-          location: "Los Angeles - 6th Area",
-          comments: [
-            {
-              author: "charliePie",
-              text: "Duuuuude he already played, you missed it..."
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: "SheffieldFest",
-      date: "12/06/2019",
-      image: "http://google.io/picture.jpeg",
-      organiser: "93729347235",
-      location: "Sheffield",
-      posts: [
-        {
-          text: "Yoooo! Here watching Drake in the diamond, looking gooooood!",
-          image: "http://borja.leiva/image.jpeg",
-          author: "charliePie",
-          date: "12/05/2019 - 15:30",
-          location: "Sheffield - The Diamond",
-          comments: [
-            {
-              author: "borjadotai",
-              text: "I know right? Pretty sick!"
-            }
-          ]
-        }
-      ]
-    }
-  ];
+  {
+    name: "Coachella",
+    date: "12/05/2019",
+    image:
+      "https://datatransmission.co/wp-content/uploads/2018/06/festival-720x394.jpg",
+    organiser: "93729347234",
+    location: "Los Angeles",
+    posts: [
+      {
+        text: "Lol I just lost my cap, anybody seen it?",
+        image: "http://borja.leiva/image.jpeg",
+        author: "borjadotai",
+        date: "12/05/2019 - 17:30",
+        location: "Los Angeles - 3rd Area",
+        comments: [
+          {
+            author: "hasanasim",
+            text: "Yo yo I saw it in Michelles crib"
+          },
+          {
+            author: "michelle23",
+            text: "Yeah its here! Come get it."
+          }
+        ]
+      },
+      {
+        text:
+          "What time is Travis playing at? Completely lost track of time...",
+        image: "http://borja.leiva/image.jpeg",
+        author: "hasanasim",
+        date: "12/05/2019 - 14:30",
+        location: "Los Angeles - 6th Area",
+        comments: [
+          {
+            author: "charliePie",
+            text: "Duuuuude he already played, you missed it..."
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "SheffieldFest",
+    date: "12/06/2019",
+    image: "http://google.io/picture.jpeg",
+    organiser: "93729347235",
+    location: "Sheffield",
+    posts: [
+      {
+        text: "Yoooo! Here watching Drake in the diamond, looking gooooood!",
+        image: "http://borja.leiva/image.jpeg",
+        author: "charliePie",
+        date: "12/05/2019 - 15:30",
+        location: "Sheffield - The Diamond",
+        comments: [
+          {
+            author: "borjadotai",
+            text: "I know right? Pretty sick!"
+          }
+        ]
+      }
+    ]
+  }
+];
 
 /**
  * called by the HTML onload
  * showing any cached forecast data and declaring the service worker
  */
 function initEvents() {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('./service-worker.js')
-            .then(function () {
-                console.log('Service Worker Registered');
-            })
-            .catch (function (error){
-                console.log('Service Worker NOT Registered '+ error.message);
-            });
-    }
-    //check for support
-    if ('indexedDB' in window) {
-        initDatabase();
-    }
-    else {
-        console.log('This browser doesn\'t support IndexedDB');
-    }
-    loadData();
-    //for (index in data)
-        //storeCachedEventData(data[index]);
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then(function() {
+        console.log("Service Worker Registered");
+      })
+      .catch(function(error) {
+        console.log("Service Worker NOT Registered " + error.message);
+      });
+  }
+  //check for support
+  if ("indexedDB" in window) {
+    initDatabase();
+  } else {
+    console.log("This browser doesn't support IndexedDB");
+  }
+  loadData();
 }
 
 /**
  * given the list of cities created by the user, it will retrieve all the data from
  * the server (or failing that) from the database
  */
-function loadData(){
-    refreshEventList();
-    pullFromDatabase();
+function loadData() {
+  refreshEventList();
+  pullFromDatabase();
 }
-function loadEvent(id){
+function loadEvent(id) {
   initDatabase();
   getDataById(id);
 }
 
 ///////////////////////// INTERFACE MANAGEMENT ////////////
 
-
 /**
  * given the forecast data returned by the server,
  * it adds a row of weather forecasts to the results div
  * @param dataR the data returned by the server:
  * class WeatherForecast{
-  *  constructor (location, date, forecast, temperature, wind, precipitations) {
-  *    this.location= location;
-  *    this.date= date,
-  *    this.forecast=forecast;
-  *    this.temperature= temperature;
-  *    this.wind= wind;
-  *    this.precipitations= precipitations;
-  *  }
-  *}
+ *  constructor (location, date, forecast, temperature, wind, precipitations) {
+ *    this.location= location;
+ *    this.date= date,
+ *    this.forecast=forecast;
+ *    this.temperature= temperature;
+ *    this.wind= wind;
+ *    this.precipitations= precipitations;
+ *  }
+ *}
  */
-function addToResults(dataR) {
-    if (document.getElementById('results') != null) {
-        const row = document.createElement('div');
-        // appending a new row
-        document.getElementById('results').appendChild(row);
-        // formatting the row by applying css classes
-        row.classList.add('card');
-        row.classList.add('my_card');
-        row.classList.add('bg-faded');
-        // the following is far from ideal. we should really create divs using javascript
-        // rather than assigning innerHTML
-        const cardBlock = document.createElement("div");
-        id= String(dataR.id);
-        row.innerHTML = "<div class='card-block'>" +
-            "<div class='row'>" +
-            "<div class='col-xs-2'><h4 class='card-title'><a id="+id+ " href=/events/"+id + " onClick=test(this)>" + dataR.name + "</a></h4></div>" +
-            "<div class='col-xs-2'></div></div></div>";
-    }
+function addToResults(data) {
+  console.log("borja", data);
+  if (document.getElementById("events") != null) {
+    const row = document.createElement("div");
+    document.getElementById("events").appendChild(row);
+    row.innerHTML =
+      "<a href={`./events/${data.id}`}>" +
+      "<img src={`data.image`} class='st-image' />" +
+      "<span class='sti-title'>" +
+      data.name +
+      "</span>" +
+      "</a>";
+  }
 }
 
 function addToEvent(dataR) {
-  if (document.getElementById('eventData') != null) {
-      const row = document.createElement('div');
-      // appending a new row
-      document.getElementById('eventData').appendChild(row);
-      // formatting the row by applying css classes
-      row.classList.add('card');
-      row.classList.add('my_card');
-      row.classList.add('bg-faded');
-      // the following is far from ideal. we should really create divs using javascript
-      // rather than assigning innerHTML
-      const cardBlock = document.createElement("div");
-      console.log(cardBlock);
-      var i;
-      posts = "";
-      for (i=0;i<dataR.posts.length;i++){
-        posts = posts +"<br> "+ String(dataR.posts[i].author) + "<br> " + String(dataR.posts[i].text);
-      }
-      row.innerHTML =  dataR.id+" <br>Name is " +dataR.name+ "<br>Location is " + dataR.location +
-         "<br> Organiser is " + dataR.organiser + posts;
-        
+  if (document.getElementById("eventData") != null) {
+    const row = document.createElement("div");
+    // appending a new row
+    document.getElementById("eventData").appendChild(row);
+    // formatting the row by applying css classes
+    row.classList.add("card");
+    row.classList.add("my_card");
+    row.classList.add("bg-faded");
+    // the following is far from ideal. we should really create divs using javascript
+    // rather than assigning innerHTML
+    const cardBlock = document.createElement("div");
+    console.log(cardBlock);
+    var i;
+    posts = "";
+    for (i = 0; i < dataR.posts.length; i++) {
+      posts =
+        posts +
+        "<br> " +
+        String(dataR.posts[i].author) +
+        "<br> " +
+        String(dataR.posts[i].text);
     }
+    row.innerHTML =
+      dataR.id +
+      " <br>Name is " +
+      dataR.name +
+      "<br>Location is " +
+      dataR.location +
+      "<br> Organiser is " +
+      dataR.organiser +
+      posts;
+  }
 }
-function test(data){
+function test(data) {
   console.log(String(data.id));
 }
 /* function for when new event entry is made */
 function newEvent() {
-    var formArray= $("form").serializeArray();
-    var data={};
-    for (index in formArray){
-        data[formArray[index].name]= formArray[index].value;
-    }
-    sendAjaxQuery('/create_event', data);
-    event.preventDefault();
+  var formArray = $("form").serializeArray();
+  var data = {};
+  for (index in formArray) {
+    data[formArray[index].name] = formArray[index].value;
+  }
+  sendAjaxQuery("/create_event", data);
+  event.preventDefault();
 }
 
 /* send request to server */
 function sendAjaxQuery(url, data) {
-    $.ajax({
-        url: url ,
-        data: data,
-        dataType: 'json',
-        type: 'POST',
-        success: function (response) {
-            addToResults(response);
-            storeCachedEventData(response);
-            if (document.getElementById('offline_div')!=null)
-                    document.getElementById('offline_div').style.display='none';
-        },
-        error: function (xhr, status, error) {
-            showOfflineWarning();
-            var offlineEventList = JSON.parse(localStorage.getItem('offline_events'));
-            offlineEventList.push(data);
-            newEventList = offlineEventList;
-            localStorage.setItem("offline_events", JSON.stringify(newEventList));
-            const dvv= document.getElementById('offline_div');
-            if (dvv!=null)
-                    dvv.style.display='block';
-        }
-    });
+  $.ajax({
+    url: url,
+    data: data,
+    dataType: "json",
+    type: "POST",
+    success: function(response) {
+      addToResults(response);
+      storeCachedEventData(response);
+      if (document.getElementById("offline_div") != null)
+        document.getElementById("offline_div").style.display = "none";
+    },
+    error: function(xhr, status, error) {
+      showOfflineWarning();
+      var offlineEventList = JSON.parse(localStorage.getItem("offline_events"));
+      offlineEventList.push(data);
+      newEventList = offlineEventList;
+      localStorage.setItem("offline_events", JSON.stringify(newEventList));
+      const dvv = document.getElementById("offline_div");
+      if (dvv != null) dvv.style.display = "block";
+    }
+  });
 }
-
 
 /**
  * When the client gets off-line, it shows an off line warning to the user
  * so that it is clear that the data is stale
  */
-window.addEventListener('offline', function(e) {
+window.addEventListener(
+  "offline",
+  function(e) {
     // Queue up events for server.
     localStorage.setItem("offline_events", JSON.stringify([]));
     console.log("You are offline");
     showOfflineWarning();
-}, false);
+  },
+  false
+);
 
 /**
  * When the client gets online, it hides the off line warning
  */
-window.addEventListener('online', function(e) {
+window.addEventListener(
+  "online",
+  function(e) {
     // Resync data with server.
     console.log("You are online");
     hideOfflineWarning();
-    var offlineEventList = JSON.parse(localStorage.getItem('offline_events'));
+    var offlineEventList = JSON.parse(localStorage.getItem("offline_events"));
     for (index in offlineEventList) {
-        sendAjaxQuery('create_event', offlineEventList[index]);
+      sendAjaxQuery("create_event", offlineEventList[index]);
     }
     localStorage.clear();
     loadData();
-}, false);
+  },
+  false
+);
 
-
-function showOfflineWarning(){
-    if (document.getElementById('offline_div')!=null)
-        document.getElementById('offline_div').style.display='block';
+function showOfflineWarning() {
+  if (document.getElementById("offline_div") != null)
+    document.getElementById("offline_div").style.display = "block";
 }
 
-function hideOfflineWarning(){
-    if (document.getElementById('offline_div')!=null)
-        document.getElementById('offline_div').style.display='none';
+function hideOfflineWarning() {
+  if (document.getElementById("offline_div") != null)
+    document.getElementById("offline_div").style.display = "none";
 }
-
 
 /**
  * it shows the city list in the browser
  */
 function showEventForm() {
-    if (document.getElementById('city_list')!=null)
-        document.getElementById('city_list').style.display = 'block';
+  if (document.getElementById("city_list") != null)
+    document.getElementById("city_list").style.display = "block";
 }
 
 /**
  * refreshes div section
  */
-function refreshEventList(){
-    if (document.getElementById('results')!=null)
-        document.getElementById('results').innerHTML='';
+function refreshEventList() {
+  if (document.getElementById("results") != null)
+    document.getElementById("results").innerHTML = "";
 }
--- public/scripts/database.js
+++ public/scripts/database.js
@@ -1,33 +1,26 @@
-////////////////// DATABASE //////////////////
-// the database receives from the server the following structure
-/** class WeatherForecast{
- *  constructor (location, date, forecast, temperature, wind, precipitations) {
- *    this.location= location;
- *    this.date= date,
- *    this.forecast=forecast;
- *    this.temperature= temperature;
- *    this.wind= wind;
- *    this.precipitations= precipitations;
- *  }
- *}
- */
 var dbPromise;
 
-const MANIFEST_DB_NAME= 'manifest_db'
-const MANIFEST_STORE_NAME = 'store_manifest'
+const MANIFEST_DB_NAME = "manifest_db";
+const MANIFEST_STORE_NAME = "store_manifest";
 
 /**
  * it inits the database
  */
-function initDatabase(){
-    dbPromise = idb.openDb(MANIFEST_DB_NAME, 1, function(upgradeDb) {
-        if (!upgradeDb.objectStoreNames.contains(MANIFEST_STORE_NAME)) {
-            var events = upgradeDb.createObjectStore(MANIFEST_STORE_NAME, {keyPath: 'id', autoIncrement: true});
-            events.createIndex('name', 'name', {unique: false, multiEntry: true});
-            events.createIndex('location', 'location', {unique: false, multiEntry: true});
-            events.createIndex('date', 'date', {unique: false, multiEntry: true});
-        }
-    });
+function initDatabase() {
+  dbPromise = idb.openDb(MANIFEST_DB_NAME, 1, function(upgradeDb) {
+    if (!upgradeDb.objectStoreNames.contains(MANIFEST_STORE_NAME)) {
+      var events = upgradeDb.createObjectStore(MANIFEST_STORE_NAME, {
+        keyPath: "id",
+        autoIncrement: true
+      });
+      events.createIndex("name", "name", { unique: false, multiEntry: true });
+      events.createIndex("location", "location", {
+        unique: false,
+        multiEntry: true
+      });
+      events.createIndex("date", "date", { unique: false, multiEntry: true });
+    }
+  });
 }
 
 /**
@@ -35,37 +28,43 @@ function initDatabase(){
  * @param city
  * @param forecastObject
  */
-function storeCachedEventData(eventObject) { // need to add actual eventobject as per above
-    console.log('inserting: '+JSON.stringify(eventObject));
-    if (dbPromise) {
-        dbPromise.then(async db => {
-            var tx = db.transaction(MANIFEST_STORE_NAME, 'readwrite');
-            var store = tx.objectStore(MANIFEST_STORE_NAME);
-            await store.put(eventObject);
-            return tx.complete;
-        }).then(function () {
-            console.log('added item to the store! '+ JSON.stringify(eventObject));
-        }).catch(function (error) {
-            console.log("Tried everything to catch this and gave up")
-        });
-    }
-    else localStorage.setItem(eventObject.name, JSON.stringify(eventObject));
+function storeCachedEventData(eventObject) {
+  // need to add actual eventobject as per above
+  console.log("inserting: " + JSON.stringify(eventObject));
+  if (dbPromise) {
+    dbPromise
+      .then(async db => {
+        var tx = db.transaction(MANIFEST_STORE_NAME, "readwrite");
+        var store = tx.objectStore(MANIFEST_STORE_NAME);
+        await store.put(eventObject);
+        return tx.complete;
+      })
+      .then(function() {
+        console.log("added item to the store! " + JSON.stringify(eventObject));
+      })
+      .catch(function(error) {
+        console.log("Tried everything to catch this and gave up");
+      });
+  } else localStorage.setItem(eventObject.name, JSON.stringify(eventObject));
 }
 
 function pullFromDatabase() {
-    if (dbPromise) {
-        dbPromise.then(function (db) {
-            return db.transaction(db.objectStoreNames).objectStore(MANIFEST_STORE_NAME).getAll();
-        }).then(function (allData) {
-            if (allData && allData.length>0){
-                for (var elem of allData)
-                    addToResults(elem);
-            }
-        });
-    }
-    else {
-        console.log("need to do this as well");
-    }
+  if (dbPromise) {
+    dbPromise
+      .then(function(db) {
+        return db
+          .transaction(db.objectStoreNames)
+          .objectStore(MANIFEST_STORE_NAME)
+          .getAll();
+      })
+      .then(function(allData) {
+        if (allData && allData.length > 0) {
+          for (var elem of allData) addToResults(elem);
+        }
+      });
+  } else {
+    console.log("need to do this as well");
+  }
 }
 
 /**
@@ -75,70 +74,67 @@ function pullFromDatabase() {
  * @returns {*}
  */
 function getCachedEventData(event) {
-    if (dbPromise) {
-        dbPromise.then(function (db) {
-            console.log('fetching: '+event);
-            var tx = db.transaction(db.objectStoreNames);
-            var store = tx.objectStore(MANIFEST_STORE_NAME);
-            var index = store.index('name');
-            return index.getAll(IDBKeyRange.only(event));
-        }).then(function (readingsList) {
-            if (readingsList && readingsList.length>0){
-                var max;
-                for (var elem of readingsList)
-                    if (!max || elem.date>max.date)
-                        max= elem;
-                if (max) addToResults(max);
-            } else {
-                const value = localStorage.getItem(event);
-                if (value == null)
-                    addToResults({event: event});
-                else addToResults(value);
-            }
-        });
-    } else {
-        const value = localStorage.getItem(event);
-        if (value == null)
-            addToResults( {city: city, date: date});
-        else addToResults(value);
-    }
+  if (dbPromise) {
+    dbPromise
+      .then(function(db) {
+        console.log("fetching: " + event);
+        var tx = db.transaction(db.objectStoreNames);
+        var store = tx.objectStore(MANIFEST_STORE_NAME);
+        var index = store.index("name");
+        return index.getAll(IDBKeyRange.only(event));
+      })
+      .then(function(readingsList) {
+        if (readingsList && readingsList.length > 0) {
+          var max;
+          for (var elem of readingsList)
+            if (!max || elem.date > max.date) max = elem;
+          if (max) addToResults(max);
+        } else {
+          const value = localStorage.getItem(event);
+          if (value == null) addToResults({ event: event });
+          else addToResults(value);
+        }
+      });
+  } else {
+    const value = localStorage.getItem(event);
+    if (value == null) addToResults({ city: city, date: date });
+    else addToResults(value);
+  }
 }
 function getDataById(id) {
-    if (dbPromise) {
-        dbPromise.then(function (db) {
-           // console.log(String(id));
-            console.log('fetching: '+event);
-            var tx = db.transaction(db.objectStoreNames);
-            var store = tx.objectStore(MANIFEST_STORE_NAME);
-            var index = store.index('name');
-            return store.getAll(IDBKeyRange.only(parseInt(id)));
-        }).then(function (readingsList) {
-            console.log(readingsList);
-            if (readingsList && readingsList.length>0){
-                var max;
-                for (var elem of readingsList)
-                    if (!max || elem.date>max.date)
-                        max= elem;
-                if (max) addToEvent(max);
-            } else {
-                const value = localStorage.getItem(event);
-                console.log(value);
-                if (value == null)
-                    addToResults({event: event});
-                else addToResults(value);
-            }
-        });
-    } else {
-        const value = localStorage.getItem(event);
-        console.log(value);
-        if (value == null)
-            addToResults( {city: city, date: date});
-        else addToResults(value);
-    }
+  if (dbPromise) {
+    dbPromise
+      .then(function(db) {
+        // console.log(String(id));
+        console.log("fetching: " + event);
+        var tx = db.transaction(db.objectStoreNames);
+        var store = tx.objectStore(MANIFEST_STORE_NAME);
+        var index = store.index("name");
+        return store.getAll(IDBKeyRange.only(parseInt(id)));
+      })
+      .then(function(readingsList) {
+        console.log(readingsList);
+        if (readingsList && readingsList.length > 0) {
+          var max;
+          for (var elem of readingsList)
+            if (!max || elem.date > max.date) max = elem;
+          if (max) addToEvent(max);
+        } else {
+          const value = localStorage.getItem(event);
+          console.log(value);
+          if (value == null) addToResults({ event: event });
+          else addToResults(value);
+        }
+      });
+  } else {
+    const value = localStorage.getItem(event);
+    console.log(value);
+    if (value == null) addToResults({ city: city, date: date });
+    else addToResults(value);
+  }
 }
 
 function getEventName(dataR) {
-    if (dataR.name == null && dataR.name === undefined)
-        return "unavailable";
-    else return dataR.name;
+  if (dataR.name == null && dataR.name === undefined) return "unavailable";
+  else return dataR.name;
 }
